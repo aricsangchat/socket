7ed15c84c843336074980742c45452b5f6fa40c8
Aric Sangchat <aric.sangchat@gmail.com>
2021-06-13 23:15:54 +0700

    updating heroku

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 4000;
-const bot = require('./bot');
-let settings = require('./settings.js');
-const helpers = require('./helpers/helpers.js');
-const tickerSettings = require('./tickerSettings.json');
+// var express = require('express');
+// var app = express();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+// var port = process.env.PORT || 4000;
+// const bot = require('./bot');
+// let settings = require('./settings.js');
+// const helpers = require('./helpers/helpers.js');
+// const tickerSettings = require('./tickerSettings.json');
 
-app.use(express.static('client'));
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-app.get('/api/get-all-tickers', function(req, res){
-  bot.allTickers();
-});
+// app.use(express.static('client'));
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+// app.get('/api/get-all-tickers', function(req, res){
+//   bot.allTickers();
+// });
 
-io.on('connection', function(socket){
-  socket.on('botLog', function(msg){
-    if (msg === 'start') {
-      tickerSettings.forEach(tickerSetting => {
-        bot.start(tickerSetting.ticker, io, bot.getAverageBuyPrice);
-        //bot.getAverageBuyPrice()
-      })
-    } else if (msg === 'stop') {
-      helpers.stopSockets(io);
-    } else {
-      settings.outputCommand(io, msg);
-    }
-    io.emit('botLog', msg);
-  });
-});
+// io.on('connection', function(socket){
+//   socket.on('botLog', function(msg){
+//     if (msg === 'start') {
+//       tickerSettings.forEach(tickerSetting => {
+//         bot.start(tickerSetting.ticker, io, bot.getAverageBuyPrice);
+//         //bot.getAverageBuyPrice()
+//       })
+//     } else if (msg === 'stop') {
+//       helpers.stopSockets(io);
+//     } else {
+//       settings.outputCommand(io, msg);
+//     }
+//     io.emit('botLog', msg);
+//   });
+// });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+// http.listen(port, function(){
+//   console.log('listening on *:' + port);
+// });
